b5780176985653b8a7042dace0aa355f90a37ba2
RYUSEULGI <71699667+RYUSEULGI@users.noreply.github.com>
2021-05-26 18:30:57 +0900

    style:템플릿 수정

---
+++ fe/src/components/Header/menuItems.js
@@ -0,0 +1,23 @@
+export const menuItems = [
+    {
+        name: 'hire-list',
+        label: '공고검색',
+    },
+    {
+        name: 'profile-list',
+        label: '배우검색',
+    },
+    {
+        name: 'actor-mypage',
+        label: '마이페이지(a)',
+    },
+    {
+        name: 'dashboard',
+        label: '마이페이지(p)',
+    },
+    {
+        name: 'https://uxtheme.net/product-support/',
+        label: 'QNA(시간남으면 하기)',
+        isExternal: true,
+    },
+];
